82ec1596547e59221c4c289aefa8ebc1ffc0edb2
Joris Moine <moine.j@sfeir.com>
2019-02-20 08:37:08 +0100

    [exercice-2-correction] Add props to component title

--- src/core/components/Title/index.jsx
+++ src/core/components/Title/index.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+const properties = {
+    title: "Hello world Game Store",
+    className: "title"
+}
+
 export const Title =  () => {
-    return <h1>Hello world Game Store</h1>
+    return <h1 className={properties.className}>{`${properties.title}: A new beginning!`}</h1>
 }

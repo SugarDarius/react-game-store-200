5c48f4bfc7610574e1c5d047695ff3b927a2db7e
Joris Moine <moine.j@sfeir.com>
2019-02-24 15:25:12 +0100

    [exo-2-cor] Initializing exercice 2 correction

---
+++ src/exercice-02-correction/index.jsx
@@ -0,0 +1,11 @@
+import '../shared/stylesheets/normalize.css';
+import './stylesheet.scss';
+
+import React from 'react';
+import { Title } from '../core/components/exercice-02-correction/index.jsx'
+
+export const Exercice02CorrectionApp = () => (
+    <main>
+        <Title />
+    </main>
+)

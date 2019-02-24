98b293b42e4a8441ce52a0d28b351d47b686487c
Joris Moine <moine.j@sfeir.com>
2019-02-24 15:51:56 +0100

    [exercice-03] Implementing exercice 03

---
+++ src/exercice-03/index.jsx
@@ -0,0 +1,12 @@
+import '../shared/stylesheets/normalize.css';
+import '../shared/stylesheets/commons.scss';
+import './stylesheet.scss';
+
+import React from 'react';
+import { Title } from '../core/components/exercice-03/index.jsx'
+
+export const Exercice03App = () => (
+    <main>
+        <Title />
+    </main>
+)

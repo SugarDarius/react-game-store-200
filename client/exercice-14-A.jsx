622e3b89041f17a261899ddbd9f5ec12493848fe
Joris Moine <moine.j@sfeir.com>
2019-02-26 14:39:17 +0100

    [exo-14-A] Add help with store

--- client/exercice-14-A.jsx
+++ client/exercice-14-A.jsx
@@ -7,11 +7,16 @@ import { AppContainer } from 'react-hot-loader';
 
 import { Exercice14AApp as App } from '../src/exercice-14-A';
 import { Context } from "../src/core/components/exercice-14-A/index";
+import { applyMiddleware, createStore } from "redux";
+import { appReducers } from "../src/core/components/exercice-14-A-correction/Reducers";
+import thunk from "redux-thunk";
 
 const DEV = process.env.NODE_ENV === 'development';
 const node = document.getElementById('react-mount-node');
 
 DEV && console.log('Rendering exercice-14-A client ...');
+
+//const store = createStore(appReducers, applyMiddleware(thunk));
 
 ReactDOM.render((
     <AppContainer>
